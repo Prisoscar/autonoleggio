5a4bc53cf4363b5e962ec91e2b57fdcd7837e2ef
Prisoscar <67907720+Prisoscar@users.noreply.github.com>
2021-03-10 14:27:13 +0100

    fix collaboratore search

--- autonoleggioSpring/src/main/java/it/mcprisa/autonoleggio/repository/RepCollaboratore.java
+++ autonoleggioSpring/src/main/java/it/mcprisa/autonoleggio/repository/RepCollaboratore.java
@@ -3,11 +3,14 @@ package it.mcprisa.autonoleggio.repository;
 import it.mcprisa.autonoleggio.model.Collaboratore;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface RepCollaboratore extends JpaRepository<Collaboratore, String>{
 
+    @Query("select t from Collaboratore t where t.nome = ?1 OR t.partitaIva = ?1")
     public List<Collaboratore> findByNomeOrPartitaIva(String criterioRicerca);
 
+    @Query("select t from Collaboratore t where t.nome LIKE  ?1 OR t.partitaIva LIKE  ?1")
     public List<Collaboratore> findByNomeLikeOrPartitaIvaLike(String string);
     
 }
